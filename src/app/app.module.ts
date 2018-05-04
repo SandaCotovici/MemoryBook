2620043f195cf8e3a9c9f97d92532d52d5135ea0
Your Name <you@example.com>
2018-05-04 22:55:36 +0300

    FIX routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { AlbumComponent } from './content/album/album.component';
 import { PhotoCollectionComponent } from './content/photo-collection/photo-collection.component';
-
-const appRoutes: Routes = [
-  { path: 'album', component: AlbumComponent},
-  { path: 'orders' , component: AlbumComponent},
-  { path: 'userManual', component: AlbumComponent },
-  { path: 'profile', component: AlbumComponent },
-  { path: 'photoCollection', component: PhotoCollectionComponent },
-  { path: '',
-    redirectTo: '/album',
-    pathMatch: 'full'
-  }
-];
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
@@ -29,11 +17,8 @@ const appRoutes: Routes = [
     PhotoCollectionComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    BrowserModule
+    BrowserModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { ModuleWithProviders } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { AlbumComponent } from './content/album/album.component';
+import { PhotoCollectionComponent } from './content/photo-collection/photo-collection.component';
+
+
+export const router: Routes = [
+  { path: '', redirectTo: 'album', pathMatch: 'full'},
+  { path: 'album', component: AlbumComponent},
+  { path: 'orders' , component: AlbumComponent},
+  { path: 'userManual', component: AlbumComponent },
+  { path: 'profile', component: AlbumComponent },
+  { path: 'photoCollection', component: PhotoCollectionComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
+// export const routes: ModuleWithProviders =  RouterModule.forRoot(router, { enableTracing: true });

3d1d7511a44e50a0c28a97a88d4c0628c5cec236
Your Name <you@example.com>
2018-05-13 12:44:28 +0300

    add user model and change

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  albums: [{type: Schema.Types.ObjectId, ref: 'Album'}]
+});
+
+mongoose.model('User', userSchema);
+
+// schema.plugin(mongooseUniqueValidator);
+//
+// module.exports = mongoose.model('User', schema);

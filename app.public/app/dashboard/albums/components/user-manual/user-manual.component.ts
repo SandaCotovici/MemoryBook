7cafaf717b262d5bf804767772fe26a81c9bef47
Your Name <you@example.com>
2018-05-27 21:37:41 +0300

    style user manual

--- app.public/app/dashboard/albums/components/user-manual/user-manual.component.ts
+++ app.public/app/dashboard/albums/components/user-manual/user-manual.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-manual.component.css']
 })
 export class UserManualComponent implements OnInit {
+  IsHidden1 = true;
+  IsHidden2 = true;
+  IsHidden3 = true;
+  IsHidden4 = true;
+  IsHidden5 = true;
 
+  onSelect1() {
+    this.IsHidden1 = !this.IsHidden1;
+  }
+  onSelect2() {
+    this.IsHidden2 = !this.IsHidden2;
+  }
+  onSelect3() {
+    this.IsHidden3 = !this.IsHidden3;
+  }
+  onSelect4() {
+    this.IsHidden4 = !this.IsHidden4;
+  }
+  onSelect5() {
+    this.IsHidden5 = !this.IsHidden5;
+  }
   constructor() { }
 
   ngOnInit() {
